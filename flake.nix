b88f1cfeabb6a39d18e0c6d0d0ee3cc12a40a643
hugosenari <hugosenari@gmail.com>
2022-03-12 03:24:50 -0300

    chore: re index tcl

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper for the TCL programming language'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tcl-master".url = "path:./master";
+  inputs."tcl-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tcl-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Wrapper for the TCL programming language'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tcl-master.flake = false;
+  inputs.src-tcl-master.owner = "nim-lang";
+  inputs.src-tcl-master.ref   = "refs/heads/master";
+  inputs.src-tcl-master.repo  = "tcl";
+  inputs.src-tcl-master.type  = "github";
+  
+  inputs."nimrod".dir   = "nimpkgs/n/nimrod";
+  inputs."nimrod".owner = "riinr";
+  inputs."nimrod".ref   = "flake-pinning";
+  inputs."nimrod".repo  = "flake-nimble";
+  inputs."nimrod".type  = "github";
+  inputs."nimrod".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrod".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tcl-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tcl-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
